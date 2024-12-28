9b5590d13cf91df43e54b36e487cdb9c764985f1
integrate360 <integrate360@gmail.com>
2024-12-28 17:09:55 +0530

    remove consoles

--- config/db.js
+++ config/db.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const connectDB = async () => {
   try {
-    console.log("MongoDB URI:", process.env.MongoDBURI);  // Debugging: Ensure URI is loaded correctly
     await mongoose.connect(process.env.MongoDBURI);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
-    process.exit(1);  // Exit process with failure
+    process.exit(1);  
   }
 };
 
