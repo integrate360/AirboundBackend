2e0b639b0149857b6455662a14ab39dddd9ece59
integrate360 <integrate360@gmail.com>
2024-12-28 16:51:46 +0530

    add bd url require('dotenv').config();

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 const connectDB = async () => {
   try {
-    console.log("MongoDB URI:", process.env.DB);  // Debugging: Ensure URI is loaded correctly
-    await mongoose.connect(process.env.DB);
+    console.log("MongoDB URI:", process.env.MongoDBURI);  // Debugging: Ensure URI is loaded correctly
+    await mongoose.connect(process.env.MongoDBURI);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
