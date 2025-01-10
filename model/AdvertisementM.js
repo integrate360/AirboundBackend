046eccb46dadc42235421e474923fbfd2bf43231
pratikravindramane <pratik.mane7727@gmail.com>
2025-01-10 15:58:50 +0530

    banner schema changed

--- model/AdvertisementM.js
+++ model/AdvertisementM.js
@@ -3,9 +3,6 @@ const mongoose = require("mongoose"); // Erase if already required
 // Declare the Schema of the Mongo model
 var AdvertisementSchema = new mongoose.Schema(
   {
-    link: {
-      type: String,
-    },
     class: { type: mongoose.Schema.Types.ObjectId, ref: "Class" },
     name: {
       type: String,
